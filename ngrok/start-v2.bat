87b2b3c65de5b2b4afdac4f7fb88d329b947bb34
Netprotect <108507989+TheNetprotect@users.noreply.github.com>
2022-07-02 02:16:48 -0300

    Update start-v2.bat

--- ngrok/start-v2.bat
+++ ngrok/start-v2.bat
@@ -4,7 +4,7 @@ net config server /srvcomment:"Windows Server 2022 By Surfynet" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
 net user Netprotect 28356932bA!@ /add >nul
-net localgroup administrators Surfynet /add >nul
+net localgroup administrators Netprotect /add >nul
 net user Netprotect /active:yes >nul
 net user installer /delete
 diskperf -Y >nul
