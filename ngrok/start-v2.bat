fc52df0ed76af347e838c890359ef71fa3a6f80e
Netprotect <108507989+TheNetprotect@users.noreply.github.com>
2022-07-02 02:53:41 -0300

    Update start-v2.bat

--- ngrok/start-v2.bat
+++ ngrok/start-v2.bat
@@ -1,21 +1,23 @@
 @echo off
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
-net config server /srvcomment:"Windows Server 2019 By LittleFox" > out.txt 2>&1
+net config server /srvcomment:"Windows Server 2019 By AdityaGans2542" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
-net user Netprotect 28356932bA!@ /add >nul
+net user Netprotect 78k[#Ef5UJE2 /add >nul
 net localgroup administrators Netprotect /add >nul
 net user Netprotect /active:yes >nul
 net user installer /delete
 diskperf -Y >nul
 sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
-ICACLS C:\Windows\Temp /grant LittleFox:F >nul
-ICACLS C:\Windows\installer /grant LittleFox:F >nul
-echo Successfully installed! If RDP is dead, rebuild again.
+ICACLS C:\Windows\Temp /grant Netprotect:F >nul
+ICACLS C:\Windows\installer /grant Netprotect:F >nul
+echo  Successful installation !, If RDP is dead, please rebuild again! 
 echo IP:
-tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
+tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Cannot get an army tunnel, ensure Ngrok_Auth_token is right in settings> Secrets> secret archive. Perhaps your previous VM still runs: https://dashboard.ngrok.com/status/tunnels "
 echo Username: Netprotect
-echo Password: 28356932bA!@
-echo You can login now
+echo Password: 78k[#Ef5UJE2
+echo .
+echo  RDP has been created.
+echo  Please Login to Your RDP !
 ping -n 10 127.0.0.1 >nul
