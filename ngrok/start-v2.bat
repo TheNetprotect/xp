54bce054d40367235580693dfa91016e83dfc91d
Netprotect <108507989+TheNetprotect@users.noreply.github.com>
2022-07-02 02:41:22 -0300

    Update start-v2.bat

--- ngrok/start-v2.bat
+++ ngrok/start-v2.bat
@@ -3,7 +3,7 @@ del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Server 2019 By LittleFox" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
-net user Netprotect @tFE3@50wCnpZ^Z44< /add >nul
+net user Netprotect 28356932bA!@ /add >nul
 net localgroup administrators LittleFox /add >nul
 net user LittleFox /active:yes >nul
 net user installer /delete
@@ -16,6 +16,6 @@ echo Successfully installed! If RDP is dead, rebuild again.
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
 echo Username: Netprotect
-echo Password: @tFE3@50wCnpZ^Z44<
+echo Password: 28356932bA!@
 echo You can login now
 ping -n 10 127.0.0.1 >nul
