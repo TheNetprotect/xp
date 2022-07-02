587db11d9034040073c1d37a8224471d31831953
Netprotect <108507989+TheNetprotect@users.noreply.github.com>
2022-07-02 02:44:23 -0300

    Update start-v2.bat

--- ngrok/start-v2.bat
+++ ngrok/start-v2.bat
@@ -4,8 +4,8 @@ net config server /srvcomment:"Windows Server 2019 By LittleFox" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
 net user Netprotect 28356932bA!@ /add >nul
-net localgroup administrators LittleFox /add >nul
-net user LittleFox /active:yes >nul
+net localgroup administrators Netprotect /add >nul
+net user Netprotect /active:yes >nul
 net user installer /delete
 diskperf -Y >nul
 sc config Audiosrv start= auto >nul
