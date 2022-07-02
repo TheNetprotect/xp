c2b31aa7f7fc67f8a36b944c9abd9210ef59caa7
Netprotect <108507989+TheNetprotect@users.noreply.github.com>
2022-07-02 02:16:30 -0300

    Update start-v2.bat

--- ngrok/start-v2.bat
+++ ngrok/start-v2.bat
@@ -10,8 +10,6 @@ net user installer /delete
 diskperf -Y >nul
 sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
-ICACLS C:\Windows\Temp /grant Surfynet:F >nul
-ICACLS C:\Windows\installer /grant Surfynet:F >nul
 echo Successfully installed! If RDP is dead, rebuild again.
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
