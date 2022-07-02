c60f590b28b960a97fea27300e73641666036283
Netprotect <108507989+TheNetprotect@users.noreply.github.com>
2022-07-02 02:24:17 -0300

    Update start-v2.bat

--- ngrok/start-v2.bat
+++ ngrok/start-v2.bat
@@ -1,19 +1,21 @@
 @echo off
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
-net config server /srvcomment:"Windows Server 2022 By Surfynet" > out.txt 2>&1
+net config server /srvcomment:"Windows Server 2022 By Netprotect" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
-net user Netprotect 28356932bA!@ /add >nul
+net user Netprotect 1VwpY/^<ZdwN|5B<3H /add >nul
 net localgroup administrators Netprotect /add >nul
 net user Netprotect /active:yes >nul
 net user installer /delete
 diskperf -Y >nul
 sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
+ICACLS C:\Windows\Temp /grant LittleFox:F >nul
+ICACLS C:\Windows\installer /grant LittleFox:F >nul
 echo Successfully installed! If RDP is dead, rebuild again.
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
 echo Username: Netprotect
-echo Password: 28356932bA!@
+echo Password: 1VwpY/^<ZdwN|5B<3H
 echo You can login now
 ping -n 10 127.0.0.1 >nul
